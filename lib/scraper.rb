b86ea450c835e6303490487b12e4fa2b25f6fc25
Marc Steele <marcsteele2014@gmail.com>
2020-11-10 19:11:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/")
+    doc = Nokogiri::HTML(html)
+    binding.pry
+    doc.css("h4").text
+    doc.css("p").text
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
